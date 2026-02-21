5acb96a4301c0cf8b5ac7125c7fd3dbc70a27e4d
Miyelani Maluleke <miyelanim@contrusion.co.za>
2026-02-21 14:16:26 +0200

    MMaluleke: - Migrate project to Spring Boot. - Updated Maven dependencies for Spring Boot (stater-test and boot-starter). - Added Spring Boot application entry point - Added a Spring-boot test to ensure application context loads without exceptions

---
+++ java/src/test/java/org/ikigaidigital/TimeDepositApplicationTest.java
@@ -0,0 +1,14 @@
+package org.ikigaidigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeDepositApplicationTest {
+
+    @Test
+    void contextLoads() {
+        // This test checks if the Spring application context loads successfully -- Helps ensure that all beans are properly configured and there are no issues with the application setup.
+        // Test passes if the application context loads without exceptions
+    }
+}

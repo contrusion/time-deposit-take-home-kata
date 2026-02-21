c8a69dcecab8759de3c3a76c556e73cc18b12245
Miyelani Maluleke <miyelanim@contrusion.co.za>
2026-02-21 14:40:02 +0200

    MMaluleke: - Updated the pom to support spring-boot web features - Added a REST API Controller Class with two Stub methods: 1 for updating deposits, and the other for getting all Time Deposits.

---
+++ java/src/main/java/org/ikigaidigital/controllers/TimeDepositController.java
@@ -0,0 +1,29 @@
+package org.ikigaidigital.controllers;
+
+import org.ikigaidigital.TimeDeposit;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/time-deposits")
+public class TimeDepositController {
+
+    @PostMapping("/update-deposits")
+    public List<TimeDeposit> updateDeposits(@RequestBody List<TimeDeposit> deposits) {
+        
+        //Todo: I will introduce the time deposit calculator service to update the balance of the 
+        // time deposits based on the business rules defined in the TimeDepositCalculator class. 
+        // This will allow us to calculate the new balances for each time deposit based on 
+        // their respective plan types and durations.
+        return deposits;
+    }
+
+    @GetMapping
+    public List<TimeDeposit> getAllDeposits() {
+        
+        //Todo: I will introduce the time deposit service to fetch all time deposits from the database repository. 
+        // This will allow us to retrieve the list of time deposits stored in the database and return them to the client.
+        return new ArrayList<>();
+    }
+}

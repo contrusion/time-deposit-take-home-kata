b7662b730fc192b6fce1ecb84ddead25f1669bc3
Miyelani Maluleke <miyelanim@contrusion.co.za>
2026-02-22 16:19:51 +0200

    MMaluleke:
    - Introduced swagger-ui for API documentation.

--- java/src/main/java/org/ikigaidigital/controllers/TimeDepositController.java
+++ java/src/main/java/org/ikigaidigital/controllers/TimeDepositController.java
@@ -5,6 +5,9 @@ import org.ikigaidigital.services.TimeDepositService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 @RestController
 @RequestMapping("/api/time-deposits")
@@ -28,13 +31,32 @@ public class TimeDepositController {
      * @param depositDtos the list of TimeDepositDto objects to update or save
      * @return the list of updated or newly created TimeDepositDto objects
      */
+
+    @Operation(
+            summary = "Update or create TimeDeposit entities",
+            description = "Updates the provided list of TimeDepositDto objects. If an object has an id that does not exist (or id is 0), it will be saved as a new entity. Otherwise, the existing entity will be updated."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Successfully updated or created deposits"),
+            @ApiResponse(responseCode = "400", description = "Invalid input")
+    })
     @PostMapping("/update-deposits")
     public List<TimeDepositDto> updateDeposits(@RequestBody List<TimeDepositDto> depositDtos) {
         return timeDepositService.updateDeposits(depositDtos);
     }
+
+
+    @Operation(
+            summary = "Get all TimeDeposit entities",
+            description = "Retrieves all TimeDepositDto objects from the database."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved all deposits")
+    })
 
     @GetMapping
     public List<TimeDepositDto> getAllDeposits() {
         return timeDepositService.getAllDeposits();
     }
 }
+
